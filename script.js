5e48ae4c24da49dd4fb716d13a9f8807fac2ed12
yagox <yago.ramiresx@gmail.com>
2022-03-25 08:44:20 -0300

    Inclusão da metodologia BEM no HTML, CSS Mobile adicionado e desktop corrigido para responsivo, ajuste nos nomes das classes no script

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const question = document.querySelectorAll(".question");
-const answer = document.querySelectorAll(".answer");
-const rowDown = document.querySelectorAll(".seta-bx");
-const rowUp = document.querySelectorAll(".seta-cm");
+const question = document.querySelectorAll(".faq__question");
+const answer = document.querySelectorAll(".faq__answer");
+const row = document.querySelectorAll(".faq__row");
 
 for (let i = 0; i < question.length; i++) {
   question[i].addEventListener("click", function () {
-    if (answer[i].classList.contains("hidden")) {
-      answer[i].classList.remove("hidden");
-      question[i].classList.add("bold");
-      rowDown[i].classList.add("hidden");
-      rowUp[i].classList.remove("hidden");
+    if (answer[i].classList.contains("faq__answer--hidden")) {
+      answer[i].classList.remove("faq__answer--hidden");
+      question[i].classList.add("faq__question--bold");
+      row[i].style.transform = "rotate(180deg)";
     } else {
-      answer[i].classList.add("hidden");
-      question[i].classList.remove("bold");
-      rowDown[i].classList.remove("hidden");
-      rowUp[i].classList.add("hidden");
+      answer[i].classList.add("faq__answer--hidden");
+      question[i].classList.remove("faq__question--bold");
+      row[i].style.transform = "rotate(0deg)";
     }
   });
 }
+
+// const question = document.querySelectorAll(".question");
+
+// question.forEach();
